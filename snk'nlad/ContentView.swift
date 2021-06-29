c6c0c170e3d15492650e253e14e3435ee4403f9b
falcon 草 <falcon.lee1011@gmail.com>
2021-06-30 02:51:18 +0800

    Initial Commit

---
+++ snk'nlad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  snk'nlad
+//
+//  Created by falcon on 2021/6/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ snk'nlad/snk_nladApp.swift
@@ -0,0 +1,17 @@
+//
+//  snk_nladApp.swift
+//  snk'nlad
+//
+//  Created by falcon on 2021/6/30.
+//
+
+import SwiftUI
+
+@main
+struct snk_nladApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
